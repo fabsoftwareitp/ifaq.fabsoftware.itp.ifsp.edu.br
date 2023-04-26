0cbdb0fbcc79ab86b317ff1ed9d7951773d9c2d6
leonardoDluz <thestuff.luz@gmail.com>
2023-04-26 15:32:10 -0300

    adição da pagina inicial e pagination

--- swiper.js
+++ swiper.js
@@ -3,5 +3,9 @@ const configSwiper = new Swiper('.swiper', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
     }
   });
