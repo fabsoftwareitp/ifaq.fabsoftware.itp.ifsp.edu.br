0abf818459d937aaefb938c9a6080fa15495d5ec
leonardoDluz <thestuff.luz@gmail.com>
2023-04-27 21:39:20 -0300

    implementação da visualização das respostas

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const caixaPerguntas = document.querySelector(".swiper-wrapper");
 const botaoParar = document.querySelector("#button-stop");
+
 async function lerJson() {
   const arquivoJson = await fetch("data.json");
   let data = await arquivoJson.json();
   data = Array.from(data);
 
-  data.forEach((e, key) => {
+  data.forEach((e) => {
     let newDiv = document.createElement("div");
-    newDiv.innerText = e.pergunta;
+    let textoPergunta = document.createElement("span");
+    let textoResposta = document.createElement("span");
+     
+    textoPergunta.innerText = e.pergunta;
+    textoResposta.innerText = e.resposta;
 
     newDiv.classList.add("swiper-slide");
-    newDiv.classList.add("pgt");
+    textoPergunta.classList.add("texto-pergunta");
+    textoResposta.classList.add("texto-resposta"); 
 
+    newDiv.appendChild(textoPergunta);  
+    newDiv.appendChild(textoResposta);  
+    caixaPerguntas.appendChild(newDiv);
+    
     newDiv.onclick = function () {
+  
       // recebe o texto da pergunta
-      let utterance = new SpeechSynthesisUtterance(newDiv.textContent);
+      let utterance = new SpeechSynthesisUtterance(e.pergunta);
       // define o pitch e o rate da voz
       utterance.pitch = 1.2;
       utterance.rate = 1.5;
@@ -27,15 +38,25 @@ async function lerJson() {
       utterance.rate = 1.5;
       // fala a resposta
       speechSynthesis.speak(utterance);
+
+
+      utterance.onstart = function () {
+        textoPergunta.classList.add("texto-desativo");
+        textoResposta.classList.add("texto-ativo");   
+      }
+      
+      utterance.onend = function () {
+        textoPergunta.classList.remove("texto-desativo");
+        textoResposta.classList.remove("texto-ativo");
+      }
+      
       botaoParar.onclick = function(){
         speechSynthesis.cancel();
+        textoPergunta.classList.remove("texto-desativo");
+        textoResposta.classList.remove("texto-ativo");  
       };
     };
-
-
-
-
-    caixaPerguntas.appendChild(newDiv);
+    
   });
 }
 
