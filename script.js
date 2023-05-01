9fdab84d30e60af6a4bd6b3bea860da579291a22
Martins-pedro23 <martinsdeoliveira.pedro2@gmail.com>
2023-05-01 14:06:34 -0300

    add: implementacao funcao next slide

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ async function lerJson() {
   data.forEach((e) => {
     let newDiv = document.createElement("div");
     let texto = document.createElement("span");
-     
+
     texto.innerText = e.pergunta;
 
     newDiv.classList.add("swiper-slide");
     texto.classList.add("texto-slide");
 
-    newDiv.appendChild(texto);  
- 
+    newDiv.appendChild(texto);
+
     caixaPerguntas.appendChild(newDiv);
-    
+
     newDiv.onclick = function () {
-  
+      speechSynthesis.cancel();
       // recebe o texto da pergunta
       let utterance = new SpeechSynthesisUtterance(e.pergunta);
       // define o pitch e o rate da voz
@@ -34,35 +34,40 @@ async function lerJson() {
       utterance.pitch = 1.2;
       utterance.rate = 1.5;
       // fala a resposta
-      speechSynthesis.speak(utterance);
-
-      swiper.on('activeIndexChange', () => {
+      
+      swiper.on("activeIndexChange", () => {
         speechSynthesis.cancel();
-        newDiv.classList.remove('animate__animated', 'animate__fadeInDown');
-        newDiv.classList.add('animate__animated', 'animate__fadeInUp');
+        newDiv.classList.remove("animate__animated", "animate__fadeInDown");
+        newDiv.classList.add("animate__animated", "animate__fadeInUp");
         texto.innerText = e.pergunta;
       });
 
       utterance.onstart = function () {
-        newDiv.classList.remove('animate__animated', 'animate__fadeInUp');
-        newDiv.classList.add('animate__animated', 'animate__fadeInDown');
-        texto.innerText = e.resposta;   
-      }
-      
+        newDiv.classList.remove("animate__animated", "animate__fadeInUp");
+        newDiv.classList.add("animate__animated", "animate__fadeInDown");
+        texto.innerText = e.resposta;
+      };
+
       utterance.onend = function () {
-        newDiv.classList.remove('animate__animated', 'animate__fadeInDown');
-        newDiv.classList.add('animate__animated', 'animate__fadeInUp');
-        texto.innerText = e.pergunta;   
-      }
+        newDiv.classList.remove("animate__animated", "animate__fadeInDown");
+        newDiv.classList.add("animate__animated", "animate__fadeInUp");
+        texto.innerText = e.pergunta;
+      };
       
-      botaoParar.onclick = function(){
+      //proximo slide
+      speechSynthesis.speak(utterance);
+      utterance.addEventListener('end', () => {
+        swiper.slideNext();
+      });
+
+      //botão de parar
+      botaoParar.onclick = function () {
         speechSynthesis.cancel();
-        newDiv.classList.remove('animate__animated', 'animate__fadeInDown');
-        newDiv.classList.add('animate__animated', 'animate__fadeInUp');
-        texto.innerText = e.pergunta; 
+        newDiv.classList.remove("animate__animated", "animate__fadeInDown");
+        newDiv.classList.add("animate__animated", "animate__fadeInUp");
+        texto.innerText = e.pergunta;
       };
     };
-    
   });
 }
 
