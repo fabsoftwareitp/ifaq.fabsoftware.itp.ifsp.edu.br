38b53a3f1d34c5941146cbd003cb346c1dfdc9e2
MarcosFabSoftware <leoleohenrique601@gmail.com>
2024-05-23 17:23:29 -0300

    Modificação nas animações para usar, as animação do próprio AMP Stories

--- script.js
+++ script.js
@@ -1,41 +1,3 @@
-function typeWrite(elemento, delay = 0) {
-  const textoArray = elemento.innerHTML.split('');
-  elemento.innerHTML = ' ';
-  textoArray.forEach(function (letra, i) {
-    setTimeout(function () {
-      elemento.innerHTML += letra;
-    }, delay + 75 * i);
-  });
-}
-
-const pergunta = document.querySelector('.pergunta');
-typeWrite(pergunta);
-
-const resposta = document.querySelector('.resposta');
-setTimeout(() => {
-  typeWrite(resposta);
-}, 9300);
-
-function fadeInElement(element) {
-  var opacity = 0;
-  var intervalId = setInterval(function() {
-    opacity += 0.1;
-    element.style.opacity = opacity;
-
-    if (opacity >= 1) {
-      clearInterval(intervalId);
-    }
-  }, 100);
-}
-
-var revealfadeins = document.querySelectorAll('#revealfadein');
-
-revealfadeins.forEach(function(revealfadein) {
-  setTimeout(function() {
-    fadeInElement(revealfadein);
-  }, 9000);
-});
-
 const slides = document.querySelectorAll('.slide');
   let currentIndex = 0;
 
@@ -55,15 +17,3 @@ const slides = document.querySelectorAll('.slide');
     currentIndex = (currentIndex + 1) % slides.length;
     updateSlides();
   }, 3000);
-
-document.body.addEventListener("click", (e) => {
-  const container = document.querySelector(".i-amphtml-system-layer-host");
-  const element = container.shadowRoot.querySelector(".i-amphtml-story-system-layer");
-  const state = element.hasAttribute("paused");
-
-  if(state) {
-    alert("tocando");
-  } else {
-    alert("pausado");
-  }
-})
