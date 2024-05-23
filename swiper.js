9c36ccc7ec8738a71a22a8ccf9fb6aa392f9e6f1
Danilo Camargo Bueno <danilocbueno@gmail.com>
2024-05-23 13:52:16 -0300

    Revert "Pagina inicial"

---
+++ swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    }
+  });
